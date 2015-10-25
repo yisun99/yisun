963b4dc7e79a834fc367b571c453ca43f5cae6f5
Alex Clemmer <clemmer.alexander@gmail.com>
2015-10-25 09:38:25 -0700

    Windows: Implemented `stout/os/windows/ls.hpp`.
    Review: https://reviews.apache.org/r/39802

--- 3rdparty/libprocess/3rdparty/stout/include/stout/os/windows/ls.hpp
+++ 3rdparty/libprocess/3rdparty/stout/include/stout/os/windows/ls.hpp
@@ -16,12 +16,58 @@
 #include <list>
 #include <string>
 
+#include <stout/try.hpp>
+
+#include <stout/os/stat.hpp>
+
+#include <stout/internal/windows/dirent.hpp>
+
 
 namespace os {
 
 inline Try<std::list<std::string>> ls(const std::string& directory)
 {
-  UNIMPLEMENTED;
+  DIR* dir = opendir(directory.c_str());
+
+  if (dir == NULL) {
+    return ErrnoError("Failed to opendir '" + directory + "'");
+  }
+
+  // NOTE: unlike the POSIX spec, our implementation of `dirent` is
+  // constant-sized. In particular `dirent.d_name` is always of size
+  // `MAX_PATH`, which is not guaranteed by the POSIX spec. Since we only need
+  // to support our implementation of `dirent`, we can greatly simplify the
+  // allocation logic here vs. the allocation logic in the POSIX-specific
+  // version of `ls.hpp`.
+  dirent* temp = (dirent*) malloc(sizeof(dirent));
+
+  if (temp == NULL) {
+    // Preserve malloc error.
+    ErrnoError error("Failed to allocate directory entries");
+    closedir(dir);
+    return error;
+  }
+
+  std::list<std::string> result;
+  struct dirent* entry;
+  int error;
+
+  while ((error = readdir_r(dir, temp, &entry)) == 0 && entry != NULL) {
+    if (strcmp(entry->d_name, ".") == 0 || strcmp(entry->d_name, "..") == 0) {
+      continue;
+    }
+    result.push_back(entry->d_name);
+  }
+
+  free(temp);
+  closedir(dir);
+
+  if (error != 0) {
+    errno = error;
+    return ErrnoError("Failed to read directories");
+  }
+
+  return result;
 }
 
 } // namespace os {
