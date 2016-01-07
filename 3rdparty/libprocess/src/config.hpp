9957d78d4429650ef257447988fac3686e37268c
Daniel Pravat <dpravat@outlook.com>
2016-01-07 11:15:02 -0800

    Windows: Add socket flags.

--- 3rdparty/libprocess/src/config.hpp
+++ 3rdparty/libprocess/src/config.hpp
@@ -53,4 +53,14 @@
 #endif
 #endif /* __FreeBSD__ */
 
+#ifdef __WINDOWS__
+typedef int socklen_t
+#ifndef MSG_NOSIGNAL
+#define MSG_NOSIGNAL 0
+#endif
+#ifndef SOL_TCP
+#define SOL_TCP SOL_SOCKET
+#endif
+#endif /* __WINDOWS__ */
+
 #endif // __CONFIG_HPP__
