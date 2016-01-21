21f601703ad0d8b153aa8d1397a50dbf53be98b2
Daniel Pravat <dpravat@outlook.com>
2016-01-20 18:45:08 -0800

    libprocess:libevent.cpp

--- 3rdparty/libprocess/src/libevent.cpp
+++ 3rdparty/libprocess/src/libevent.cpp
@@ -10,7 +10,11 @@
 // See the License for the specific language governing permissions and
 // limitations under the License
 
-#include <unistd.h>
+#ifdef __WINDOWS__
+
+#else
+  #include <unistd.h>
+#endif
 
 #include <mutex>
 
