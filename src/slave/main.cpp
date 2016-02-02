67d2508dc28c9510abdbd1466fd77c80c497255f
Alex Naparu <alex.naparu@microsoft.com>
2016-02-02 14:43:47 -0800

    Windows: WinSock setup and cleanup routines.

--- src/slave/main.cpp
+++ src/slave/main.cpp
@@ -83,10 +83,77 @@ void version()
   cout << "mesos" << " " << MESOS_VERSION << endl;
 }
 
+// Returns `EXIT_SUCCESS` (0) on success, `EXIT_FAILURE` otherwise and logs the
+// reason for failure.
+int initialize()
+{
+  // Initialize WinSock (request version 2.2).
+  WORD requestedVersion = MAKEWORD(2, 2);
+  WSADATA data;
+
+  const int result = ::WSAStartup(requestedVersion, &data);
+  if (result != 0) {
+    const int error = ::WSAGetLastError();
+    cerr << "Could not initialize WinSock, error code : " << error << endl;
+    return EXIT_FAILURE;
+  }
+
+  // Check that the WinSock version we got back is 2.2 or higher.
+  // The high-order byte specifies the minor version number.
+  if (LOBYTE(data.wVersion) < 2 ||
+     (LOBYTE(data.wVersion) == 2 && HIBYTE(data.wVersion) != 2)) {
+    cerr << "Incorrect WinSock version found : " << LOBYTE(data.wVersion)
+        << "." << HIBYTE(data.wVersion) << endl;
+
+    // WinSock was initialized, we just didn't like the version, so we need to
+    // clean up.
+    if (::WSACleanup() != 0) {
+      const int error = ::WSAGetLastError();
+      cerr << "Could not cleanup WinSock, error code : " << error << endl;
+    }
+
+    return EXIT_FAILURE;
+  }
+
+  return EXIT_SUCCESS;
+}
+
+int cleanup()
+{
+  // Cleanup WinSock. Wait for any outstanding socket operations to complete
+  // before exiting. Retry for a maximum of 10 times at 1 second intervals.
+  int retriesLeft = 10;
+
+  while (retriesLeft > 0) {
+    const int result = ::WSACleanup();
+    if (result != 0) {
+      const int error = ::WSAGetLastError();
+      if (error != WSANOTINITIALISED) {
+        cerr << "WinSock was not initialized" << endl;
+        return EXIT_SUCCESS;
+      }
+
+      cerr << "Could not cleanup WinSock, error code : " << error << endl;
+
+      // Wait for any blocking calls to complete and retry after 1 second.
+      if (error == WSAEINPROGRESS) {
+        cerr << "Waiting for outstanding WinSock calls to complete." << endl;
+        ::Sleep(1000);
+        retriesLeft--;
+      }
+    }
+  }
+
+  return EXIT_SUCCESS;
+}
 
 int main(int argc, char** argv)
 {
   GOOGLE_PROTOBUF_VERIFY_VERSION;
+
+  if (::initialize()) {
+    return EXIT_FAILURE;
+  }
 
   slave::Flags flags;
   flags.initialize();
@@ -321,6 +388,8 @@ int main(int argc, char** argv)
   delete detector.get();
 
   delete containerizer.get();
+
+  cleanup();
 
   return EXIT_SUCCESS;
 }
